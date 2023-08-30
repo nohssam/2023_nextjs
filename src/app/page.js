729840ae92cca4749501c73501548c894e50e2c8
nohssam73 <nohssam73@gmail.com>
2023-08-30 10:04:03 +0900

    기본문법완성

--- src/app/page.js
+++ src/app/page.js
@@ -6,8 +6,8 @@ export default function Home() {
           <h1>WelCome</h1>
           <h2>Hello, WEB!!</h2>
           {/* 해당 이미지를 따로 import 하지 않으면 width, height를 반드시 지정하자 */}
-          <p><Image src="/images/tree-1.jpg" width={100} height={100} /></p>
-          <p><Image src={img1}  width={100} height={100} /></p>
+          <p><Image src="/images/tree-1.jpg" alt="" width={100} height={100} /></p>
+          <p><Image src={img1} alt="" width={100} height={100} /></p>
         </>
       );
 }
